daa055812e69544c3f90822a8c11387a69a5b276
Juan Carlos Ortiz <57382762+JUANORTIZR@users.noreply.github.com>
2022-07-13 16:13:45 -0500

    cors

--- src/main/java/com/juandevs/prue11/controller/UsuarioController.java
+++ src/main/java/com/juandevs/prue11/controller/UsuarioController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,9 @@ import com.juandevs.prue11.entity.Usuario;
 import com.juandevs.prue11.service.UsuarioServiceImpl;
 
 @RestController
+
+@CrossOrigin(origins = "http://localhost:4200", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT,
+    RequestMethod.DELETE})
 
 @RequestMapping("/api/usuario")
 public class UsuarioController {
